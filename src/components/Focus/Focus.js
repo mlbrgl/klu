733e9b7443dcf413f1088728630c03fdc79e0c2b
Matthieu Bergel <hello@matthieubergel.org>
2018-02-25 18:29:13 +0100

    Simplified contenteditable logic (https://codepen.io/mlbrgl/pen/QQVMRP)

--- src/components/Focus/Focus.js
+++ src/components/Focus/Focus.js
@@ -1,43 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import FocusItem from './FocusItem/FocusItem';
 import './Focus.css';
 
-class Focus extends Component {
-
-  shouldComponentUpdate = (nextProps, nextState) => {
-    // we shouldn't update the component if the number of items hasn't changed,
-    // since it means that the state was updated as part as user editing the content
-    // without adding or removing elements. Otherwise, we are messing with the
-    // caret position, on top of executing useless renders.
-    // @TODO ... except if the user clicked on the first element to clear its content
-    // without deleting it.
-    // console.log(nextProps.items[0].value === '')
-    // return this.props.items.length !== nextProps.items.length || (nextProps.items.length === 1 && nextProps.items[0].value === '') ;
-    return true;
-  }
-
-  render() {
-    // console.log('in focus render')
-    return (
-      <div className="focus">
-        {this.props.items.map((item, index) => {
-          return(
-            <FocusItem
-              changed={this.props.changed.bind(null, item.id)}
-              deleted={this.props.deleted.bind(null, item.id)}
-              keypressed={this.props.keypressed.bind(null, item.id)}
-              key={item.id}
-              id={item.id}
-              index={index}
-              focus={this.props.focus}
-            >
-              {item.value}
-            </FocusItem>)
-          }
-        )}
-      </div>
-    )
-  }
+const focus =  (props) => {
+  return (
+    <div className="focus">
+      {props.items.map((item, index) => {
+        return(
+          <FocusItem
+            changed={props.changed.bind(null, item.id)}
+            deleted={props.deleted.bind(null, item.id)}
+            keypressed={props.keypressed.bind(null, item.id)}
+            key={item.id}
+            id={item.id}
+            index={index}
+            focus={props.focus}
+          >
+            {item.value}
+          </FocusItem>)
+        }
+      )}
+    </div>
+  )
 }
 
-export default Focus;
+export default focus;
--- src/components/Focus/FocusItem/FocusItem.js
+++ src/components/Focus/FocusItem/FocusItem.js
@@ -29,7 +29,8 @@ class FocusItem extends Component {
     // From https://github.com/lovasoa/react-contenteditable/blob/master/src/react-contenteditable.js
     // "We need not rerender if the change of props simply reflects the user's edits.
     // Rerendering in this case would make the cursor/caret jump"
-    return nextProps.index === 0 && nextProps.children === '';
+    // return nextProps.index === 0 && nextProps.children === '';
+    return nextProps.children !== this.ref.innerHTML;
   }
 
   render() {
@@ -38,7 +39,6 @@ class FocusItem extends Component {
         <span className="icon-right-open-big" onClick={this.props.deleted}></span>
         <div
           className="edit"
-          dangerouslySetInnerHTML={{__html: this.props.children}}
           onKeyDown={this.props.keypressed}
           onInput={this.props.changed}
           ref={el => this.ref = el}
@@ -50,16 +50,19 @@ class FocusItem extends Component {
   }
 
   componentDidMount() {
+    // Whole story here https://codepen.io/mlbrgl/pen/QQVMRP
+    this.updateInnerHtml();
     // A bit of an overkill but covers both initial load and subsequent adds.
     // Will focus on the last element during the initial load sequence.
     this.setCaretEnd(this.ref);
   }
 
   componentDidUpdate() {
-    // No idea why this is required. Even though the render does happen, and this.props.children
-    // is saying holding the right value at console.log() time, the DOM is not being updated.
-    // Demo of the issue: https://codepen.io/mlbrgl/pen/QQVMRP
-    // Arrived at the same solution as https://github.com/lovasoa/react-contenteditable/blob/master/src/react-contenteditable.js
+    // Whole story here https://codepen.io/mlbrgl/pen/QQVMRP
+    this.updateInnerHtml();
+  }
+
+  updateInnerHtml() {
     this.ref.innerHTML = this.props.children;
   }
 
