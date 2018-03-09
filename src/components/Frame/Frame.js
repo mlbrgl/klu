5466e28d060ee4e9fffe26ad9041d39a57fd2755
Matthieu Bergel <hello@matthieubergel.org>
2018-03-09 23:50:40 +0100

    Fix background image rotation on each render

--- src/components/Frame/Frame.js
+++ src/components/Frame/Frame.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { getRandomIntInclusive } from '../../helpers/helpers'
 import styles from './Frame.module.css'
 
-const frame = (props) => {
-  const style = {
-    backgroundImage: "url('/images/" + getRandomIntInclusive(1,45) + ".jpg')"
+class Frame extends Component {
+
+  componentWillMount() {
+    this.style = {backgroundImage: "url('" + process.env.PUBLIC_URL + "/images/" + getRandomIntInclusive(1,45) + ".jpg')"}
   }
 
-  return (
-    <div className={props.focus ? styles.focused : styles.frame} style={style}>
+  render() {
+    return (
+      <div className={this.props.focus ? styles.focused : styles.frame} style={this.style}>
       <div className={styles.inner}>
-        {props.children}
+      {this.props.children}
       </div>
-    </div>
-  )
+      </div>
+    )
+  }
 }
 
-export default frame;
+export default Frame;
